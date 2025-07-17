15e91ae433aad1ae8836193083df3e92bf69b4f8
Sharad <ksfbd100@gmail.com>
2025-07-17 11:22:58 +0530

    This is my first commit

---
+++ src/main.java
@@ -0,0 +1,6 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!\nMy name is Sharad Sharma");
+        System.out.println("This is my DSA journey");
+    }
+}
